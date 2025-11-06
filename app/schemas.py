65d6a1bd745cafdde9f1d5097a0e5ce3962d5b33
PyWendi <rakotondranaivogilbert21@gmail.com>
2025-11-07 02:21:28 +0300

    fix : fix

--- app/schemas.py
+++ app/schemas.py
@@ -206,7 +206,7 @@ class DocumentRequestUpdate(BaseModel):
 
 class DocumentRequestCLientUpdate(BaseModel):
     pere: Optional[str] = None # pending, validate, refused
-    mere: Optional[bool] = None
+    mere: Optional[str] = None
     categorie_id: Optional[int] = None
     infosupps: Optional[List[InfoSuppSchema]] = None
 
